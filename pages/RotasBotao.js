b7d74faad8467c92b37117f19f0c6aff138a75a6
LEONARDO MOCKAITES <leonardo.mockaites@senacsp.edu.br>
2022-07-13 11:50:28 -0300

    Atualizando

--- pages/RotasBotao.js
+++ pages/RotasBotao.js
@@ -4,20 +4,17 @@ import { createStackNavigator } from '@react-navigation/stack'
 
 const navBotao = createStackNavigator();
 
-import ListarFrutas from './Frutas/ListarFrutas';
-import CadastrarFruta from './Frutas/CadastrarFrutas';
-import AlterarFrutas from './Frutas/AlterarFrutas';
+import ListarFrutas from '../pages/Frutas/ListarFrutas';
+import CadastrarFruta from '../pages/Frutas/CadastrarFrutas';
+import AlterarFrutas from '../pages/Frutas/AlterarFrutas';
 
 export default function RotasBotao(){
 
 return(
     <navBotao.Navigator>
-
-    <navBotao.Screen name="Lista" component={ListaFrutas} options ={{headerShow:false}}/>
-    <navBotao.Screen name="Cadastrar" component={ListaFrutas} options ={{headerShow:false}}/>
-    <navBotao.Screen name="Alterar" component={ListaFrutas} options ={{headerShow:false}}/>  
-    
+        <navBotao.Screen name="Lista" component={ListaFrutas} options ={{headerShow:false}} />
+        <navBotao.Screen name="Cadastrar" component={ListaFrutas} options ={{headerShow:false}} />
+        <navBotao.Screen name="Alterar" component={ListaFrutas} options ={{headerShow:false}} />  
     </navBotao.Navigator>
      );
-
   }
